e9ac1df9e0d1e352e03359bfead9b2c148059aea
Alex Beh <chunjye.beh@hmgics.com>
2025-05-13 10:53:33 +0800

    rename to ros2_example

--- example/ros2_example/synchronizer_component.cpp
+++ example/ros2_example/synchronizer_component.cpp
@@ -17,14 +17,17 @@ public:
         RCLCPP_INFO(get_logger(), "Initializing SynchronizerComponent...");
 
         // Set up the serial connection
-        synchronizer_.SetSerialLink("/dev/ttyACM0", 460800);
+        // synchronizer_.SetUsbLink("/dev/ttyACM0", 460800);
 
-        // Optionally: SetNetLink or UseMvCam
-        // synchronizer_.SetNetLink("192.168.1.188", 8888);
-        // synchronizer_.UseMvCam();
+        synchronizer_.SetNetLink("192.168.1.188", 8888);
 
+        // 2.配置同步接口
+        // auto mv_cam = std::make_shared<CustomCam>();
+        // mv_cam->SetParams({{"camera_1", CAM_1},{"camera_2", CAM_2},});
+        // synchronizer_.UseSensor(mv_cam);
+
+        // 3.开启同步
         synchronizer_.Start();
-        Synchronizer::PrintSummary();
 
         // Setup a timer instead of while loop
         timer_ = this->create_wall_timer(1s, [this]() {
